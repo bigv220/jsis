403548a8831116734dd5c8061c00dea861851bc0
sophia <sophia@sinodev.com>
2014-03-10 18:24:54 +0800

    添加财付通插件

---
+++ catalog/view/theme/default/template/payment/checkout.php
@@ -0,0 +1,81 @@
+<?php
+// Heading 
+$_['heading_title']                  = '结帐';
+
+// Text
+$_['text_cart']                    = '购物车';
+$_['text_checkout_option']           = '1: 结帐确认';
+$_['text_checkout_account']          = '2: 帐户 &amp; 帐单细节';
+$_['text_checkout_payment_address']  = '2: 帐单地址';
+$_['text_checkout_shipping_address'] = '3: 配送地址';
+$_['text_checkout_shipping_method']  = '4: 配送方式';
+$_['text_checkout_payment_method']   = '5: 付款方式';
+$_['text_checkout_confirm']          = '6: 最后确认';
+$_['text_modify']                    = '修改 &raquo;';
+$_['text_new_customer']              = '加入会员';
+$_['text_returning_customer']        = '会员登录';
+$_['text_checkout']                  = '非会员结帐区:';
+$_['text_i_am_returning_customer']   = '会员请由此登录';
+$_['text_register']                  = '加入会员';
+$_['text_guest']                     = '直接购买';
+$_['text_register_account']          = '加入会员可享有更快捷的购物，可随时查看订单状态和订购纪录，以及更多的会员专属服务.';
+$_['text_forgotten']                 = '忘记密码';
+$_['text_your_details']              = '会员资料';
+$_['text_your_address']              = '通讯地址';
+$_['text_your_password']             = '密码';
+$_['text_agree']                     = '我已阅读并同意 <a class="thickbox" href="%s" alt="%s"><b>%s</b></a> 的内容';
+$_['text_address_new']               = '其它地址';
+$_['text_address_existing']          = '选择其它地址';
+$_['text_shipping_method']           = '请选择商品配送方式';
+$_['text_payment_method']            = '请选择付款方式';
+$_['text_comments']                  = '备注(关于此笔订单):';
+
+// Column
+$_['column_name']                    = '商品名称';
+$_['column_model']                   = '型号';
+$_['column_quantity']                = '数量';
+$_['column_price']                   = '单价';
+$_['column_total']                   = '金额';
+
+// Entry
+$_['entry_email_address']            = 'E-Mail:';
+$_['entry_email']                    = 'E-Mail:';
+$_['entry_password']                 = '密码:';
+$_['entry_confirm']                  = '确认密码:';
+$_['entry_firstname']                = '姓名:';
+$_['entry_lastname']                 = '姓氏:';
+$_['entry_telephone']                = '手机:';
+$_['entry_fax']                      = '电话:';
+$_['entry_company']                  = '公司:';
+$_['entry_address_1']                = '地址 1:';
+$_['entry_address_2']                = '地址 2:';
+$_['entry_postcode']                 = '邮政编码:';
+$_['entry_city']                     = '城市:';
+$_['entry_country']                  = '国家:';
+$_['entry_zone']                     = '地区:';
+$_['entry_newsletter']               = '我愿意收到 %s 的最新商品讯息.';
+$_['entry_shipping'] 	             = '商品配送地址和帐单地址相同.';
+
+// Error
+$_['error_warning']                  = '请选择其他付款方式，如果无适用于您的付款方式请 <a href="%s">与商家联系</a>.';
+$_['error_minimum']                  = '%s 最少需订购 %s 件!';	
+$_['error_login']                    = '很抱歉: 电子邮箱或密码错误';
+$_['error_exists']                   = '很抱歉: 这个邮箱已被注册，请换一个邮箱!';
+$_['error_firstname']                = '名字必须在1到16个字符之间!';
+$_['error_lastname']                 = '姓氏必须在1到16个字符之间!';
+$_['error_email']                    = '电子邮箱格式错误!';
+$_['error_telephone']                = '电话号码必须在2到16个字符!';
+$_['error_password']                 = '密码必须在2到10个字符之间!';
+$_['error_confirm']                  = '确认密码与输入密码不相同!';
+$_['error_address_1']                = '地址必须在2到64个字符之间!';
+$_['error_city']                     = '城市名必须在1到64个字符之间!';
+$_['error_postcode']                 = '邮政编码必须在1到10个字符之间!';
+$_['error_country']                  = '请选择国家!';
+$_['error_zone']                     = '请选择地区或省份!';
+$_['error_agree']                    = '很抱歉: 您尚未同意 %s 条款!';
+$_['error_address']                  = '地址尚未选取!';
+$_['error_shipping']                 = '请选择配送方式!';
+$_['error_no_shipping']              = '很抱歉: 目前没有配送方式可选择. 请 <a href="%s">与我们联系</a> !';
+$_['error_payment']                  = '请选择付款方式!';
+$_['error_no_payment']               = '很抱歉: 目前没有付款方式可选择. 请 <a href="%s">与我们联系</a> !';
+?>
