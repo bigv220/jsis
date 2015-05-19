d61591b6ea8cdf2992369231803850b0ec5c5932
Yangliyu <405100835@qq.com>
2015-05-19 15:46:47 +0800

    首页添加4行产品分类

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -11,7 +11,191 @@ class ControllerCommonHome extends Controller {
 		} else {
 			$this->template = 'default/template/common/home.tpl';
 		}
-		
+
+        $this->load->model('catalog/product');
+
+        $this->load->model('tool/image');
+
+
+
+        /***********首页添加珠宝类产品***********/
+        $data_zhubao = array(
+            'filter_category_id' => 39,
+            'filter_filter'      => '',
+            'sort'               => '',
+            'order'              => DESC,
+            'start'              => 0,
+            'limit'              => 4
+        );
+        $results_zhubao = $this->model_catalog_product->getProducts($data_zhubao);
+
+        foreach ($results_zhubao as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+            } else {
+                $image = false;
+            }
+
+            if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $special = false;
+            }
+
+            $this->data['products_zhubao'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'rating'      => $result['rating'],
+                'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+                'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url)
+            );
+        }
+        /******************END***************/
+
+
+
+        /***********首页添加床上用品产品***********/
+        $data_bed = array(
+            'filter_category_id' => 18,
+            'filter_filter'      => '',
+            'sort'               => '',
+            'order'              => DESC,
+            'start'              => 0,
+            'limit'              => 4
+        );
+        $results_bed = $this->model_catalog_product->getProducts($data_bed);
+
+        foreach ($results_bed as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+            } else {
+                $image = false;
+            }
+
+            if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $special = false;
+            }
+
+            $this->data['products_bed'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'rating'      => $result['rating'],
+                'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+                'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url)
+            );
+        }
+        /******************END***************/
+
+        /***********首页添加日化品产品***********/
+        $data_daily = array(
+            'filter_category_id' => 15,
+            'filter_filter'      => '',
+            'sort'               => '',
+            'order'              => DESC,
+            'start'              => 0,
+            'limit'              => 4
+        );
+        $results_daily = $this->model_catalog_product->getProducts($data_daily);
+
+        foreach ($results_daily as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+            } else {
+                $image = false;
+            }
+
+            if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $special = false;
+            }
+
+            $this->data['products_daily'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'rating'      => $result['rating'],
+                'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+                'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url)
+            );
+        }
+        /******************END***************/
+
+        /***********首页添加澳洲代购产品***********/
+        $data_daigou = array(
+            'filter_category_id' => 81,
+            'filter_filter'      => '',
+            'sort'               => '',
+            'order'              => DESC,
+            'start'              => 0,
+            'limit'              => 4
+        );
+        $results_daigou = $this->model_catalog_product->getProducts($data_daigou);
+
+        foreach ($results_daigou as $result) {
+            if ($result['image']) {
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+            } else {
+                $image = false;
+            }
+
+            if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+                $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $price = false;
+            }
+
+            if ((float)$result['special']) {
+                $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+            } else {
+                $special = false;
+            }
+
+            $this->data['products_daigou'][] = array(
+                'product_id'  => $result['product_id'],
+                'thumb'       => $image,
+                'name'        => $result['name'],
+                'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 100) . '..',
+                'price'       => $price,
+                'special'     => $special,
+                'rating'      => $result['rating'],
+                'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+                'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url)
+            );
+        }
+        /******************END***************/
+
 		$this->children = array(
 			'common/column_left',
 			'common/column_right',
--- catalog/view/theme/default/template/common/home.tpl
+++ catalog/view/theme/default/template/common/home.tpl
@@ -1,5 +1,128 @@
 <?php echo $header; ?><?php echo $column_left; ?><?php echo $column_right; ?>
 <div id="content"><?php echo $content_top; ?>
 <h1 style="display: none;"><?php echo $heading_title; ?></h1>
+    <h1 class="general_heading">珠宝类</h1>
+
+    <div class="products_container">
+        <?php foreach ($products_zhubao as $product) { ?><div class='product_holder featured_product'>
+            <div class='product_holder_inside'>
+
+                <?php if ($product['special']) { ?>
+                <?php echo '<div class="special_promo"></div>'; ?>
+            <?php } ?>
+
+            <?php if ($product['thumb']) { ?>
+            <div class="image"><a href="<?php echo $product['href']; ?>"><img src="<?php echo $product['thumb']; ?>" alt="<?php echo $product['name']; ?>" /></a></div>
+            <?php } ?>
+            <div class="pr_info">
+                <div class="name"><a href="<?php echo $product['href']; ?>"><?php echo $product['name']; ?></a></div>
+                <?php if ($product['price']) { ?>
+                <div class="price">
+                    <?php if (!$product['special']) { ?>
+                    <?php echo $product['price']; ?>
+                    <?php } else { ?>
+                    <span class="price-old"><?php echo $product['price']; ?></span> <span class="price-new"><?php echo $product['special']; ?></span>
+                    <?php } ?>
+                </div>
+                <?php } ?>
+                <div class="cart"><a class='button' onclick="addToCart('<?php echo $product['product_id']; ?>');"><span>加入购物车</span></a></div>
+            </div>
+        </div>
+    </div><?php } ?>
+</div>
+
+<h1 style="display: none;"><?php echo $heading_title; ?></h1>
+<h1 class="general_heading">床上用品</h1>
+
+<div class="products_container">
+    <?php foreach ($products_bed as $product) { ?><div class='product_holder featured_product'>
+        <div class='product_holder_inside'>
+
+            <?php if ($product['special']) { ?>
+            <?php echo '<div class="special_promo"></div>'; ?>
+        <?php } ?>
+
+        <?php if ($product['thumb']) { ?>
+        <div class="image"><a href="<?php echo $product['href']; ?>"><img src="<?php echo $product['thumb']; ?>" alt="<?php echo $product['name']; ?>" /></a></div>
+        <?php } ?>
+        <div class="pr_info">
+            <div class="name"><a href="<?php echo $product['href']; ?>"><?php echo $product['name']; ?></a></div>
+            <?php if ($product['price']) { ?>
+            <div class="price">
+                <?php if (!$product['special']) { ?>
+                <?php echo $product['price']; ?>
+                <?php } else { ?>
+                <span class="price-old"><?php echo $product['price']; ?></span> <span class="price-new"><?php echo $product['special']; ?></span>
+                <?php } ?>
+            </div>
+            <?php } ?>
+            <div class="cart"><a class='button' onclick="addToCart('<?php echo $product['product_id']; ?>');"><span>加入购物车</span></a></div>
+        </div>
+    </div>
+</div><?php } ?>
+</div>
+
+<h1 style="display: none;"><?php echo $heading_title; ?></h1>
+<h1 class="general_heading">日化产品</h1>
+
+<div class="products_container">
+    <?php foreach ($products_daily as $product) { ?><div class='product_holder featured_product'>
+        <div class='product_holder_inside'>
+
+            <?php if ($product['special']) { ?>
+            <?php echo '<div class="special_promo"></div>'; ?>
+        <?php } ?>
+
+        <?php if ($product['thumb']) { ?>
+        <div class="image"><a href="<?php echo $product['href']; ?>"><img src="<?php echo $product['thumb']; ?>" alt="<?php echo $product['name']; ?>" /></a></div>
+        <?php } ?>
+        <div class="pr_info">
+            <div class="name"><a href="<?php echo $product['href']; ?>"><?php echo $product['name']; ?></a></div>
+            <?php if ($product['price']) { ?>
+            <div class="price">
+                <?php if (!$product['special']) { ?>
+                <?php echo $product['price']; ?>
+                <?php } else { ?>
+                <span class="price-old"><?php echo $product['price']; ?></span> <span class="price-new"><?php echo $product['special']; ?></span>
+                <?php } ?>
+            </div>
+            <?php } ?>
+            <div class="cart"><a class='button' onclick="addToCart('<?php echo $product['product_id']; ?>');"><span>加入购物车</span></a></div>
+        </div>
+    </div>
+</div><?php } ?>
+</div>
+
+<h1 style="display: none;"><?php echo $heading_title; ?></h1>
+<h1 class="general_heading">澳洲代购</h1>
+
+<div class="products_container">
+    <?php foreach ($products_daigou as $product) { ?><div class='product_holder featured_product'>
+        <div class='product_holder_inside'>
+
+            <?php if ($product['special']) { ?>
+            <?php echo '<div class="special_promo"></div>'; ?>
+        <?php } ?>
+
+        <?php if ($product['thumb']) { ?>
+        <div class="image"><a href="<?php echo $product['href']; ?>"><img src="<?php echo $product['thumb']; ?>" alt="<?php echo $product['name']; ?>" /></a></div>
+        <?php } ?>
+        <div class="pr_info">
+            <div class="name"><a href="<?php echo $product['href']; ?>"><?php echo $product['name']; ?></a></div>
+            <?php if ($product['price']) { ?>
+            <div class="price">
+                <?php if (!$product['special']) { ?>
+                <?php echo $product['price']; ?>
+                <?php } else { ?>
+                <span class="price-old"><?php echo $product['price']; ?></span> <span class="price-new"><?php echo $product['special']; ?></span>
+                <?php } ?>
+            </div>
+            <?php } ?>
+            <div class="cart"><a class='button' onclick="addToCart('<?php echo $product['product_id']; ?>');"><span>加入购物车</span></a></div>
+        </div>
+    </div>
+</div><?php } ?>
+</div>
+
 <?php echo $content_bottom; ?></div>
 <?php echo $footer; ?>
